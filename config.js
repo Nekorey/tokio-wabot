e6d4f80bb75e7b8dd320eb628e8b8c34408b37fc
Nekorey <98371243+Nekorey@users.noreply.github.com>
2022-03-02 16:14:56 +0700

    Update config.js

--- config.js
+++ config.js
@@ -43,9 +43,9 @@ fs.watchFile(file_exif, () => {
   require('./lib/exif.json')
 })
 
-global.packname = Sticker_by_Sagiri
-global.author = ©-Radi
-
+ 
+ ​global​.​packname​ ​=​ ​sticker_name 
+ ​global​.​author​ ​=​ ​sticker_author
 
 global.multiplier = 69 // The higher, The harder levelup
 
